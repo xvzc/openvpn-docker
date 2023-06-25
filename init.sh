99213c8e3bbec0a60731a95c59c64714e040f768
xvzc <dev.kwanghoo@gmail.com>
2023-06-26 02:39:44 +0900

    fix: run option

--- init.sh
+++ init.sh
@@ -27,19 +27,4 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo usermod -aG docker $USER
-
-# setup and run
-newgrp docker <<EONG
-
-docker run -v $OVPN_DATA:/etc/openvpn \
-  --rm kylemanna/openvpn ovpn_genconfig \
-  -u udp://$(curl https://whatsmyip.co | xargs)
-
-docker run -v $OVPN_DATA:/etc/openvpn \
-  --rm -it kylemanna/openvpn ovpn_initpki nopass
-
-docker run -v $OVPN_DATA:/etc/openvpn -d \
-  -p 1194:1194/udp \
-  --cap-add=NET_ADMIN kylemanna/openvpn
-
-EONG
+newgrp docker
